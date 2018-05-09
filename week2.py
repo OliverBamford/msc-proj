49de73a173c5227c0f26ec664360c7f8263bb7ed
Oliver Bamford <bamford@maths.ox.ac.uk>
2018-05-09 13:54:52 +0100

    add plots

--- week2.py
+++ week2.py
@@ -11,7 +11,15 @@ def q(u):
     
 def ue(x):
     return ((2**(m+1)-1)*x + 1)**(1/(m+1)) - 1 
-
+    
+# set up BCs on left and right
+BCtol = 1E-14
+def left_boundary(x, on_boundary):
+    return on_boundary and abs(x[0]) < BCtol
+    
+def right_boundary(x, on_boundary):
+    return on_boundary and abs(x[0]-1) < BCtol
+    
 mesh = UnitIntervalMesh(100)
 V = FunctionSpace(mesh, 'CG', 1)
 
@@ -23,20 +31,36 @@ a = inner(q(u_k)*grad(u), grad(v))*dx
 f = Constant(0.0)
 L = f*v*dx
 
-# set up BCs on left and right
-BCtol = 1E-14
-def left_boundary(x, on_boundary):
-    return on_boundary and abs(x[0]) < BCtol
-    
-def right_boundary(x, on_boundary):
-    return on_boundary and abs(x[0]-1) < BCtol
-    
+
 B1 = DirichletBC(V, Constant(0.0), left_boundary) # u(0) = 0
 B2 = DirichletBC(V, Constant(1.0), right_boundary) # u(1) = 1
 bcs = [B1, B2]
 
 
 [uDiff, exactError] = getPDEConvData(a, f, u_k, ue, mesh, 1, V, 100, 1, bcs, dispOutput = True)
+plt.figure(1)
+plt.plot(uDiff, label='Iterate difference')
+plt.plot(exactError, label='Exact error')
+plt.legend()
+V = FunctionSpace(mesh, 'CG', 2)
+
+# Define variational problem for Picard iteration
+u = TrialFunction(V)
+v = TestFunction(V)
+u_k = interpolate(Constant(0.0), V)  # previous (known) u
+a = inner(q(u_k)*grad(u), grad(v))*dx
+f = Constant(0.0)
+L = f*v*dx
+
+B1 = DirichletBC(V, Constant(0.0), left_boundary) # u(0) = 0
+B2 = DirichletBC(V, Constant(1.0), right_boundary) # u(1) = 1
+bcs = [B1, B2]
+
+[uDiff2, exactError2] = getPDEConvData(a, f, u_k, ue, mesh, 1, V, 100, 2, bcs, dispOutput = True)
+plt.figure(2)
+plt.plot(uDiff2, label='Iterate difference') 
+plt.plot(exactError2, label='Exact error')
+plt.legend()
 
 #%%************** Problem 2 **************
 
@@ -47,7 +71,7 @@ f = Expression('cos(20*x[0])*sin(15*x[1])', degree=1)
 u = TrialFunction(V)
 v = TestFunction(V)
 
-u_k = Expression('x[0] + x[1]', degree=1)
+u_k = Expression('20*(1-x[0]) * 15 * x[1]', degree=1)
 a = u_k*v*dx
 
 def ue(XY):
