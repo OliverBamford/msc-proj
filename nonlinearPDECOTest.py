0e4d3382fc870e59f2097a19b8f750887b104ce1
Alberto Paganini <paganini@maths.ox.ac.uk>
2018-06-15 11:16:53 +0100

    modifications in nonlinearPDECOTest.py

--- nonlinearPDECOTest.py
+++ nonlinearPDECOTest.py
@@ -3,44 +3,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 from fenics_adjoint import *
 
-N=100;
-mesh = UnitSquareMesh(N,N)
+#L2-tracking functional with Tykhonov regularitation
+#control is the source function of Poisson's eqn
+mesh = UnitSquareMesh(100,100)
 
+#target function
 x = SpatialCoordinate(mesh)
 ue = 10*sin(4*pi*x[0])*sin(pi*x[1])
 
+#Tykhonov regularization
 alpha = 1e-10
 
-p=1;
-V = FunctionSpace(mesh, 'CG', p)
-W = FunctionSpace(mesh, 'CG', p)
+#State space and Control space
+V = FunctionSpace(mesh, 'CG', 1)
 
 # initial guess for control
 m = interpolate(Expression("x[0]", degree=1), V)
 
-u = TrialFunction(V)
+#state equation
+u_ = TrialFunction(V)
 v = TestFunction(V)
-# solve state equation 
-a = inner(grad(u), grad(v))*dx
+a = inner(grad(u_), grad(v))*dx
 L = m*v*dx
 bc = DirichletBC(V, 0., 'on_boundary')
-# dolfin_adjoint automatically saves iterates for use in control problem
 u = Function(V, name='State')
 solve(a == L, u, bc)
 
-ud = ue#interpolate(ue, V)
-
-J_form = (0.5*inner(u-ud, u-ud))*dx + alpha/2*m**2*dx # + lmbd*(inner(grad(u), grad(v)) - m*v)*dx
-J_form_u = (0.5*inner(u-ud, u-ud))*dx
-J_form_m = m**2*dx
+#L2-tracking functional and Tikhonov regularization
+J_form = (0.5*inner(u-ud, u-ud))*dx + alpha/2*m**2*dx
 J = assemble(J_form)
+
+#dolfin-adjoint stuff
 control = Control(m)
 rf = ReducedFunctional(J, control)
 dJdm = rf.derivative()
 
-#Riesz representative
-RdJ_ = TrialFunction(V)
+#L2-Riesz representative
 RdJ = Function(V)
+RdJ_ = TrialFunction(V)
 a_R = RdJ_*v*dx
 L_R = dJdm*v*dx
 
@@ -53,22 +53,17 @@ iterTol = 1e-05; maxIter = 25; dispOutput = True
 print(str(assemble(J_form_m)))
 while Jk[-1] > iterTol and iter < maxIter:
     iter += 1
-    
-    # solve state eqn
-    solve(a == L, u, bc)
-    dJdm = rf.derivative()
-    
-    # compute Riesz rep. (step-direction)
+
+    # compute Riesz representative
     solve(a_R == L_R, RdJ)
-    # step
-    m.assign(m - 100000*RdJ, annotate=False)
-    
+
+    # update control
+    m.assign(m - 100*RdJ, annotate=False)
+
     # solve state eqn and evaluate objective
     solve(a == L, u, bc)
     Jk.append(assemble(J_form))
-    
+
     if dispOutput:
-        print ('k = ' + str(iter) + ' | J = ' + str(assemble(J_form_u)) + ' | norm(m) = ' 
+        print ('k = ' + str(iter) + ' | J = ' + str(Jk[-1]) + ' | norm(m) = ' 
             + str(norm(m, 'H1')) + ' | norm(R(dJ)) = ' + str(norm(dJdm, 'H1')))
-        #print ('k = ' + str(iter) + ' | J = ' + str(Jk[-1]) + ' | norm(m) = ' 
-        #    + str(norm(m, 'H1')) + ' | norm(R(dJ)) = ' + str(norm(dJdm, 'H1')))
