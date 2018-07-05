0da214238e206dda07345804705947f6fa2eb2fb
Oliver Bamford <bamford@maths.ox.ac.uk>
2018-07-05 18:39:15 +0100

    add calculation of (d+l)|_K

--- ern_errors_rough.py
+++ ern_errors_rough.py
@@ -13,8 +13,10 @@ def dqdu(u):
         
 N = 10
 p = 1
+d = 2 # dimension of the space
 
 mesh = UnitSquareMesh(N,N)
+mesh.init(1) # generate edges
 V = FunctionSpace(mesh, 'CR', p)
 
 # set up BCs on left and right
@@ -22,7 +24,7 @@ V = FunctionSpace(mesh, 'CR', p)
 B1 = DirichletBC(V, Constant(0.0), lambda x, on_boundary : left_boundary(x, on_boundary)) # u(0) = 0
 B2 = DirichletBC(V, Constant(1.0), lambda x, on_boundary : right_boundary(x, on_boundary)) # u(1) = 1
 
-# construct exact solution in C format
+# construct exact solution in C++ format
 uExpr = Expression('pow((pow(2,m+1) - 1)*x[0] + 1,(1/(m+1))) - 1', m = 2, degree=4)
 
 iterTol = 1.0e-5; maxIter = 25; dispOutput = True
@@ -32,20 +34,44 @@ u = TrialFunction(V)
 v = TestFunction(V)
 u_k = interpolate(Constant(0.0), V) # previous (known) u
 a = inner(q(u_k)*grad(u), grad(v))*dx
-f = Constant(0.0)
+f = Constant(1.0)
 L = f*v*dx
 
 bcs = [B1, B2] 
 
-X = SpatialCoordinate(mesh)
-CR0 = FunctionSpace(mesh, 'CR', 0)
-CR1 = FunctionSpace(mesh, 'CR', 1)
-f_h = interpolate(f, CR0)
-x_ = interpolate(Expression('x[0]', degree=1), CR1)
-d = 2 # dimension of the space
-f_hvec = f_h/d * (x - x_K) #TODO: calculate barycenters x_K
+#X = SpatialCoordinate(mesh)
+DG0 = FunctionSpace(mesh, 'DG', 0)
+DG1 = FunctionSpace(mesh, 'DG', 1)
+f_h = interpolate(f, DG0)
+x_ = interpolate(Expression('x[0]', degree=1), DG1)
+h = 1/N
 
-u = Function(V)     # new unknown function
+myCells = cells(mesh)
+centers = []
+cellList = []
+# create list of cell centers (dtype: Point)
+for k in range(0, 2*N**d):
+    cellList.append(myCells.next())
+    centers.append(cellList[k].midpoint())
+
+F = VectorFunctionSpace(mesh, 'DG', degree=0, dim=d)
+# construct f_h(x)|_K vector
+f_hvec = []
+for k in range(0, len(cellList)):
+    f_hvec.append(interpolate(Expression(['c*(x[0] - xk0)', 'c*(x[1] - xk1)'],
+                                         xk0=centers[k].array()[0],
+                                         xk1=centers[k].array()[1], 
+                                         c = f_h(centers[k])/d , degree=1), F))
+                                     
+# construct residual sum
+#k = 0
+#for K in cellList:
+#    centerK = centers[k]
+#    k +=1
+                                        
+u = Function(V)
+temp = Function(F)
+                         
 itErr = 1.0           # error measure ||u-u_k||
 iterDiffArray = []
 exactErrArray = []
@@ -64,6 +90,45 @@ while itErr > iterTol and iter < maxIter:
     iterDiffArray.append(itErr) # fill arrays with error data
     exactErrArray.append(exErr)
     
+    # construct interpolation of sigma = (1+u)^2 * grad(u) into F 
+    gu = project(grad(u), F)
+    sigma = (1+u)**2 * grad(u)
+    sigmaBar = interpolate(Expression(['((1 + u)*(1 + u) + \
+                                            (1 + u)*(1 + u))*gu[0]', 
+                                        '((1 + u)*(1 + u) + \
+                                            (1 + u)*(1 + u))*gu[1]'], 
+                                            u = u, gu = gu, degree=1), F)
+    # construct sum (second term of flux (d+l)) for each cell K
+    rSum = []
+    # find residual for each edge using 'test function trick'
+    R_eps = assemble(f_h*v*dx - inner(sigmaBar, grad(v))*dx)
+    for K in cellList:
+        myEdges = edges(K)
+        myVerts = vertices(K)
+        # create list of cells edges and vertices 
+        # (would be replaced by a loop in 3D case)
+        eps_K = [myEdges.next(), myEdges.next(), myEdges.next()]
+        a_K = [myVerts.next(), myVerts.next(), myVerts.next()]
+        # |T_e| = 1 for all internal edges
+        cardT_e = 1
+        # a_K[n] is the vertex opposite to the edge eps_K[n]
+        sumK = Function(F)
+        for i in range(0, len(eps_K)-1):
+            #TODO: implement check to see if edge is on boundary
+            R_e = R_eps[eps_K[i].index()][0] # find residual corresponding to edge
+            sumK += interpolate(Expression(['C*(x[0] - a0)',
+                                            'C*(x[1] - a1)'],
+                                            C = R_e/(cardT_e*d), 
+                                            a0 = a_K[i].point().array()[0],
+                                            a1 = a_K[i].point().array()[1],
+                                            degree=1), F)
+        rSum.append(sumK)
+    # construct flux (d+l) for each element (Eq. (6.7))
+    dpl = []
+    for k in range(0, len(f_hvec)):
+        temp.assign(-sigmaBar + f_hvec[k] - rSum[k])
+        dpl.append(temp)
+    
     if dispOutput:
         print('k = ' + str(iter) + ' | u-diff =  ' + str(itErr) + ', exact error = ' + str(exErr))
-    u_k.assign(u) # update for next ite
+    u_k.assign(u) # update for next iteration
