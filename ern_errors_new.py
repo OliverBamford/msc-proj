9975378b6548f0776645546f758855401c50a2ad
Oliver Bamford <bamford@maths.ox.ac.uk>
2018-07-21 15:12:38 +0100

    interpolate fluxes into RTN

--- ern_errors_new.py
+++ ern_errors_new.py
@@ -25,7 +25,7 @@ B1 = DirichletBC(V, Constant(0.0), lambda x, on_boundary : left_boundary(x, on_b
 B2 = DirichletBC(V, Constant(1.0), lambda x, on_boundary : right_boundary(x, on_boundary)) # u(1) = 1
 
 # construct exact solution in C++ format
-uExpr = Expression('pow((pow(2,m+1) - 1)*x[0] + 1,(1/(m+1))) - 1', m = 2, degree=4)
+uExpr = Expression('pow((pow(2,m+1) - 1)*x[0] + 1, (1/(m+1))) - 1', m = 2, degree=4)
 
 iterTol = 1.0e-11; maxIter = 25; dispOutput = True
 
@@ -41,30 +41,39 @@ bcs = [B1, B2]
 #X = SpatialCoordinate(mesh)
 DG0 = FunctionSpace(mesh, 'DG', 0)
 f_h = interpolate(f, DG0)
-#TODO: make this RTN space
-F = VectorFunctionSpace(mesh, 'CR', degree=1, dim=d)
-f_hvec = interpolate(Expression(['x[0]', 'x[1]'], degree=1), F)
+
+#TODO: implement this in RTN space
+F = VectorFunctionSpace(mesh, 'CR', 1, dim=2)
+X = MeshCoordinates(mesh)
+f_hvec = interpolate(X, F)
 f_ = np.zeros(f_hvec.vector().get_local().shape) # create np array which contains values to be assigned to f_hvec
 
 dm = F.dofmap()
 for cell in cells(mesh):
     dofs = dm.cell_dofs(cell.index())
-    f_c = f_hvec.vector().get_local()[dofs] # get np array of all dofs in cell
+    f_c = f_hvec.vector().get_local()[dofs] # get np array of all dof values in cell
     mp = cell.midpoint().array() # get midpoint of cell
     f_c[0:f_c.size/2] = f*(f_c[0:f_c.size/2] - mp[0])/d # construct f_h in cell
     f_c[f_c.size/2:f_c.size] = f*(f_c[f_c.size/2:f_c.size] - mp[1])/d # note [x,x,x,y,y,y] structure of f_c
-    f_[dofs] = f_c # place cell values back into main array       
+    f_[dofs] = f_c # place cell values back into main array
 
 f_hvec.vector().set_local(f_)
 
-F0 = VectorFunctionSpace(mesh, 'DG', degree=0, dim=d) # space for 0-order interpolants (sigma)             
+# we have constructed f_h in CR, which has dofs: pointwise evaluation
+# need to constuct f_h in RTN, which has dofs: moments of the normal component against P_{q-1} on facets
+# note that dofs are in the same locations for CR1 and RT1
+RTN = FunctionSpace(mesh, 'RT', 1)
+f_hvec = interpolate(f_hvec, RTN)
+
+F0 = VectorFunctionSpace(mesh, 'DG', degree=0, dim=d) # space for 0-order interpolants (sigma)
+FC = FunctionSpace(mesh, 'CG', 2) # space for projecting jump operator onto
 u = Function(V)
 lflux = Function(F)
 dflux = Function(F)
 discflux = Function(F)
 x_ = interpolate(Expression(['x[0]', 'x[1]'], degree=0), F) # used as 'x' vector when constructing flux
                 
-itErr = 1.0           # error measure ||u-u_k||
+itErr = 1.0 # error measure ||u-u_k||
 eta_lin = 1.
 eta_disc = 0.
 gamma_lin = 0.1
@@ -88,11 +97,11 @@ while eta_lin > gamma_lin*eta_disc and iter < maxIter:
     exactErrArray.append(exErr)
     
     # construct interpolation of sigma = (1+u)^2 * grad(u) into F 
-    # sigma^{k-1} = (1+u_k)**2 * grad(u)
-    # sigma = (1+u)**2 * grad(u)
+    # sigma^{k-1} = (1+u_k)^2 * grad(u)
+    # sigma = (1+u)^2 * grad(u)
     # sigmakBar = Pi_0 sigma^{k-1}
     # sigmaBar = Pi_0 sigma
-    gu = project(grad(u), F) #TODO: make sure this is legit
+    gu = project(grad(u), RTN) #TODO: make sure this is legit
     sigmakBar = interpolate(Expression(['((1 + u)*(1 + u) + \
                                             (1 + u)*(1 + u))*gu[0]', 
                                         '((1 + u)*(1 + u) + \
@@ -100,13 +109,13 @@ while eta_lin > gamma_lin*eta_disc and iter < maxIter:
                                             u = u_k, gu = gu, degree=0), F0)
                                             # u = u_k (previous u iterate)
     # must be interpolated into higher order CR space to be used for flux calculation
-    sigmakBar = interpolate(sigmakBar, F) 
+    sigmakBar = interpolate(sigmakBar, RTN) 
     sigmaBar = interpolate(Expression(['((1 + u)*(1 + u) + \
                                             (1 + u)*(1 + u))*gu[0]', 
                                         '((1 + u)*(1 + u) + \
                                             (1 + u)*(1 + u))*gu[1]'], 
                                             u = u, gu = gu, degree=0), F0)
-    sigmaBar = interpolate(sigmaBar, F)
+    sigmaBar = interpolate(sigmaBar, RTN)
     # construct sum (second terms Eqns (6.7) and (6.9) for each cell K
     # find residual for each edge using 'test function trick'
     R_eps = assemble(f_h*v*dx - inner(sigmakBar, grad(v))*dx)
@@ -140,6 +149,10 @@ while eta_lin > gamma_lin*eta_disc and iter < maxIter:
         r_[dofs] = r_c # place cell values back into main array
     rk.vector().set_local(rk_)
     r.vector().set_local(r_)
+    
+    # interpolate CR construction of residuals into RTN
+    rk = interpolate(rk, RTN)
+    r = interpolate(r, RTN)    
     # construct flux (d+l) for each element (Eq. (6.7))
     dflux.assign(-sigmaBar + f_hvec - r)
     lflux.assign(-sigmakBar + f_hvec - rk - dflux)
