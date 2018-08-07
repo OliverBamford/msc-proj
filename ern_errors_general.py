5126b1f1c76a024d802db24c587ad4ca8721dea4
Oliver Bamford <bamford@maths.ox.ac.uk>
2018-08-07 15:39:28 +0100

    more modularity, some bugs fixed

--- ern_errors_general.py
+++ ern_errors_general.py
@@ -28,99 +28,44 @@ def sigma_Newton(u, u_k):
     if dqdgu(u) != None: SN += grad(u - u_k)*inner(dqdgu(u_k), grad(u_k))
     return SN
 
-### MESH SETUP ###
-N = 15 # mesh fine-ness
-l = 1 # order of finite elements
-d = 2 # dimension of the space
-mesh = UnitSquareMesh(N,N)
-n = FacetNormal(mesh)
-mesh.init()
-V = FunctionSpace(mesh, 'CR', l)
-
-### BOUNDARY CONDITIONS ###
+#%% MODEL PROBLEM 2
+mesh = UnitSquareMesh(10,10)
+V = FunctionSpace(mesh, 'CR', 1)
 x, y= sym.symbols('x[0] x[1]')
 uExSym = 9./10.*((0.5)**(10./9.) - ((x - 0.5)**2 + (y - 0.5)**2)**(5./9.))
 uExpr = Expression(sym.printing.ccode(uExSym), degree=4)
-u_e = interpolate(uExpr, V)
 bcs = DirichletBC(V, uExpr, 'on_boundary')
+u0Sym = uExSym*(1 + (x-0.5)*(y-0.5))
+u_k = interpolate(Expression(sym.printing.ccode(u0Sym), degree=4), V)
+u, error_estimators = solve_2D_flux_PDE(q, Constant(2.0), 10, 10, bcs, 
+                                        dqdg = dqdgu,
+                                        u0 = Expression(sym.printing.ccode(u0Sym),degree=4), 
+                                        exact_solution = uExpr)
 
-# set up BCs on left and right
-# lambda functions ensure the boundary methods take two variables
+##%% MODEL PROBLEM 1                                     
+## set up BCs on left and right
+## lambda functions ensure the boundary methods take two variables
 #B1 = DirichletBC(V, Constant(0.0), lambda x, on_boundary : left_boundary(x, on_boundary))
 #B2 = DirichletBC(V, Constant(1.0), lambda x, on_boundary : right_boundary(x, on_boundary))
 #bcs = [B1, B2]
-#uExpr = Expression('pow((pow(2,m+1) - 1)*x[0] + 1, (1/(m+1))) - 1', m = 2, degree=4)
-#u_e = interpolate(uExpr, V)
-### INITIAL GUESS ###
-u = TrialFunction(V)
-v = TestFunction(V)
-f = Constant(2.0)
-DG0 = FunctionSpace(mesh, 'DG', 0)
-f_h = interpolate(f, DG0)
-#u_k = TrialFunction(V)
-#a0 = inner(grad(u_k), grad(v))*dx
-#L0 = f_h*v*dx
-#u_k = Function(V)
-#solve(a0 == L0, u_k, bcs)
-#u_k = interpolate(Constant(0.0), V) # previous (known) u
-u0Sym = uExSym*(1 + (x-0.3)*(y-0.3))
-u_k = interpolate(Expression(sym.printing.ccode(u0Sym), degree=4), V)
-
-### GET REF SOLUTION ###
-#u_ref = u_k
-#F = (inner((inner(grad(u_ref),grad(u_ref)))**4*grad(u_ref), grad(v)) - f_h*v)*dx
-#solve(F==0, u_ref, bcs)
-
-### PROBLEM SET UP ###
-p = 10
-qu = p/float(p-1)
-sigma_lin = lambda u, u_k: sigma_Newton(u, u_k)
-F = (inner(sigma_lin(u, u_k), grad(v)) - f_h*v)*dx
-a = lhs(F)
-L = rhs(F)
-
-f_hvec = get_fvec(f_h, mesh)
-
-u = Function(V)
-eta_lin = 1.
-eta_disc = 0.
-gamma_lin = 0.0000001
-maxIter = 25; dispOutput = True
-iterDiffArray = [0]
-exactErrArray = [0]
-iter = 0
-error_estimators = np.array([[0.,0.,0.,0.,0.]])
-JupArray = [0]
-# Begin Picard iterations
-while eta_lin > gamma_lin*eta_disc and iter < maxIter:
-    iter += 1
-    
-    solve(a == L, u, bcs, solver_parameters={"linear_solver": "lu"})
-
-    # calculate iterate difference and exact error in L2 norm
-    itErr = errornorm(u_k, u, 'H1')
-    exErr = errornorm(uExpr, u, 'H1')
-    iterDiffArray.append(itErr) # fill arrays with error data
-    exactErrArray.append(exErr)
-    
-    eta_disc, eta_lin, eta_quad, eta_osc, eta_NC = get_estimators(V, f, f_h, f_hvec, sigma, sigma_lin, u, u_k, mesh, p)
-    error_estimators = np.concatenate((error_estimators, np.array([[eta_disc, eta_lin, eta_quad, eta_osc, eta_NC]])), axis = 0)
-    JupArray.append(assemble((inner(sigma(u_e) - sigma(u), sigma(u_e) - sigma(u)))**(qu/2)*dx)**(1/qu) + error_estimators[-1,4])
-    if dispOutput:
-        print('k = ' + str(iter) + ' | u-diff =  ' + str(itErr) + 
-        ', exact error = ' + str(exErr) + ', eta_lin = ' + str(eta_lin)
-        + ' , eta_disc = ' + str(eta_disc))
-    u_k.assign(u) # update for next iteration
-
+#exact_solution = Expression('pow((pow(2,m+1) - 1)*x[0] + 1, (1/(m+1))) - 1', m = 2, degree=4)
+#u, error_estimators = solve_2D_flux_PDE(q, Constant(2.0), 10, 10, bcs, 
+#                                        dqdg = dqdgu,
+#                                        u0 = Expression(sym.printing.ccode(u0Sym),degree=4), 
+#                                        exact_solution = uExpr)
+#           
+                     
+#%% PLOTTING
+                     
 plt.figure()
 plt.semilogy(error_estimators[:,0], label='$\eta_{disc}$')
 plt.semilogy(error_estimators[:,1], label='$\eta_{lin}$')
 #plt.semilogy(error_estimators[:,2], label='$\eta_{quad}$')
-plt.semilogy(error_estimators[:,3], label='$\eta_{osc}$')
-plt.semilogy(error_estimators[:,4], label='$\eta_{NC}$')
+#plt.semilogy(error_estimators[:,3], label='$\eta_{osc}$')
+#plt.semilogy(error_estimators[:,4], label='$\eta_{NC}$')
 plt.semilogy(error_estimators[:,0:4].sum(axis=1), label='$\eta$')
-plt.semilogy(exactErrArray, label='$||u_h - u_e||_{H1}$')
-plt.semilogy(JupArray, label='$J^{up}$')
+#plt.semilogy(exactErrArray, label='$||u_h - u_e||_{H1}$')
+#plt.semilogy(JupArray, label='$J^{up}$')
 plt.xlabel('Iterations')
 plt.ylabel('$\eta$')
 plt.legend()
--- ern_functions.py
+++ ern_functions.py
@@ -36,6 +36,8 @@ def get_estimators(V, f, f_h, f_hvec, sigma, sigma_lin, u, u_k, mesh, p):
     F0 = VectorFunctionSpace(mesh, 'DG', degree=0, dim=d) # space for 0-order interpolants (sigma)
     dm = RT.dofmap()
     dm0 = F0.dofmap()
+    dmV = V.dofmap()
+    edge_dofs = dmV.entity_dofs(mesh, 1) # get dofs associated with edge indexes
     sigmakBar0 = project(sigma_lin(u, u_k), F0)
     sigmaBar0 = project(sigma(u), F0)
     dflux = Function(RT)
@@ -69,23 +71,19 @@ def get_estimators(V, f, f_h, f_hvec, sigma, sigma_lin, u, u_k, mesh, p):
         myVerts = vertices(cell)
         eps_K = [myEdges.next(), myEdges.next(), myEdges.next()]
         a_K = [myVerts.next(), myVerts.next(), myVerts.next()]
-        mp = cell.midpoint().array()
+        mp = cell.midpoint().array()[0:2] # midpoint is returned in (x,y,z) format
         eta_NCK = 0.
         # a_K[n] is the vertex opposite to the edge eps_K[n]
         for i in range(len(eps_K)):
            cardT_e = eps_K[i].entities(d).size # number of cells sharing e
-           R_e = R_eps[eps_K[i].index()][0] # find residual corresponding to edge #TODO: check this over
-           Rbar_e = Rbar_eps[eps_K[i].index()][0] # find barred residual corresponding to edge          
+           R_e = R_eps[edge_dofs[eps_K[i].index()]][0] # find residual corresponding to edge
+           Rbar_e = Rbar_eps[edge_dofs[eps_K[i].index()]][0] # find barred residual corresponding to edge          
            # find distance between all dofs on cell and vertex opposite to edge
-           #if eps_K[i].entities(d).size > 1: # if edge is internal
-           rk_c[0:rk_c.size/2] += 1./(cardT_e*d)*R_e*(x_c[0:rk_c.size/2] - mp[0])
-           rk_c[rk_c.size/2:rk_c.size] += 1./(cardT_e*d)*R_e*(x_c[rk_c.size/2:rk_c.size] - mp[1])
-           r_c[0:r_c.size/2] += 1./(cardT_e*d)*Rbar_e*(x_c[0:r_c.size/2] - mp[0])
-           r_c[r_c.size/2:r_c.size] += 1./(cardT_e*d)*Rbar_e*(x_c[r_c.size/2:r_c.size] - mp[1])
+           rk_c += 1./(cardT_e*d)*R_e*(x_c - mp.repeat(3))
+           r_c += 1./(cardT_e*d)*Rbar_e*(x_c - mp.repeat(3))
            # s = q
            mf.set_value(eps_K[i].index(), 1) # mark domain to integrate over
            eta_NCK += assemble(inner(jump(u,n), jump(u,n))**(qu/2)*dS(1, subdomain_data=mf)) / eps_K[i].length() # Lq-norm of jump along edge^q
-           #TODO: make this edge integration work
            mf.set_value(eps_K[i].index(), 0) # un-mark domain
         
         dflux_c = -sigmaBar0.vector().get_local()[dofs0].repeat(3) + f_hvec.vector().get_local()[dofs] - r_c
@@ -95,8 +93,7 @@ def get_estimators(V, f, f_h, f_hvec, sigma, sigma_lin, u, u_k, mesh, p):
         eta_NCK = eta_NCK**(1/qu)
         dflux.vector().set_local(d_)
         # add local discretisation estimator^q to total
-        eta_disc += (2**(1./p)*(assemble_local((inner(dflux+sigmaBar0,dflux+sigmaBar0))**(qu/2)*dx, cell)**(1/qu) + eta_NCK))
-        eta_disc_noNC += 2**(1./p)*assemble_local((inner(dflux+sigmaBar0,dflux+sigmaBar0))**(qu/2)*dx, cell)**(1/qu)
+        eta_disc += (2**(1./p)*(assemble_local((inner(dflux+sigmaBar0,dflux+sigmaBar0))**(qu/2)*dx, cell)**(1/qu) + eta_NCK))**qu
         eta_osc += cell.h()/np.pi*assemble_local((inner(f - f_h, f - f_h))**(qu/2)*dx, cell)
         eta_NC += eta_NCK
         rk_[dofs] = rk_c # place cell values back into main array
@@ -105,13 +102,8 @@ def get_estimators(V, f, f_h, f_hvec, sigma, sigma_lin, u, u_k, mesh, p):
     lflux.vector().set_local(l_)
     rk.vector().set_local(rk_)
     r.vector().set_local(r_)
-    # interpolate CR construction of residuals into RTN
-    #rk = interpolate(rk, RTN)
-    #r = interpolate(r, RTN)
     # compute global discretisation and quadrature estimators
     eta_disc = eta_disc**(1/qu)
-    eta_disc_noNC = eta_disc_noNC**(1/qu)
-    print(eta_disc_noNC)
     eta_NC = eta_NC**(1/qu)
     eta_quad = assemble((inner(sigma(u)-sigmaBar0,sigma(u)-sigmaBar0))**(qu/2)*dx)**(1/qu)
     eta_osc = eta_osc**(1/qu)
@@ -122,3 +114,116 @@ def get_estimators(V, f, f_h, f_hvec, sigma, sigma_lin, u, u_k, mesh, p):
     eta_lin = assemble(inner(lflux,lflux)**(qu/2)*dx)**(1/(2*qu))
     #print(project(div(lflux + dflux), RT).vector().get_local())
     return eta_disc, eta_lin, eta_quad, eta_osc, eta_NC
+
+def solve_2D_flux_PDE(q, f, N, p, bcs, dqdu = None, 
+                      dqdg = None, u0 = None, exact_solution = None, 
+                      solver = 'Newton'):
+    """
+    Solves the 2D flux-type PDE:
+    find u in L^p((0,1)x(0,1)) such that:
+    -div(q(u, grad(u))*grad(u)) = f
+    + Dirichlet BCs
+    using linear Crouzeix-Raviart finite elements
+    
+    Inputs:
+    q: function handle
+    dqdu: function handle, partial derivative dq/du
+    dqdg: function handle, partial derivative dqd(grad(u))
+    f: FEniCS function
+    N: Mesh refinement
+    p: integer
+    bcs: FEniCS Dirichlet BCs (or a tuple of Dirichlet BCs)
+    u0: UFL expression, initial guess. If not specified, solution to PDE with
+    q(u) = 1 will be used.
+    exact_solution: UFL expression for analytic solution of PDE, if applicable
+    solver: string, 'Newton' or 'Picard'
+    
+    Outputs:
+    u: FEniCS function that solves the PDE
+    error_estimators: Ern error estimators from each iteration in format
+    [eta_disc, eta_lin, eta_quad, eta_osc, eta_NC]
+    """
+    
+    ### MESH SETUP ###
+    mesh = UnitSquareMesh(N,N)
+    mesh.init()
+    V = FunctionSpace(mesh, 'CR', 1)
+    if exact_solution != None:
+        u_e = interpolate(exact_solution, V)
+    f_h = interpolate(f, FunctionSpace(mesh, 'DG', 0))
+    v = TestFunction(V)
+    
+    ### INITIAL GUESS ###
+    if u0 == None:
+        u_k = TrialFunction(V)
+        a0 = inner(grad(u_k), grad(v))*dx
+        L0 = f_h*v*dx
+        u_k = Function(V)
+        solve(a0 == L0, u_k, bcs)
+    else: u_k = interpolate(u0, V)
+    
+    ### PROBLEM SET UP ###
+    u = Function(V)
+    u.assign(u_k) # load up initial guess
+    sigma = lambda u: q(u)*grad(u) # nonlinear flux function
+    qu = p/float(p-1)
+    if solver == 'Newton':  
+        # make sure these are callable
+        if dqdu == None:
+            dqdu = lambda u : 0*u
+        if dqdg == None:
+            dqdg = lambda u : 0*u    
+        bcs_du = DirichletBC(V, Constant(0.0), 'on_boundary') # homog. bcs for Newton step
+        sigma_lin = lambda u, u_k: q(u_k)*grad(u) \
+                                    + (u - u_k)*dqdu(u_k)*grad(u_k) \
+                                    + grad(u - u_k)*inner(dqdg(u_k), grad(u_k))
+        #F = (inner(sigma_lin(u, u_k), grad(v)) - f_h*v)*dx
+        F = (inner(sigma(u), grad(v)) - f_h*v)*dx
+        du = TrialFunction(V)
+        J = derivative(F, u, du)
+        du = Function(V)
+    elif solver == 'Picard':
+        u = TrialFunction(V)
+        sigma_lin = lambda u, u_k: q(u_k)*grad(u)
+        F = (inner(sigma_lin(u, u_k), grad(v)) - f_h*v)*dx
+        a = lhs(F)
+        L = rhs(F)
+    else: print('Solver not recognised'); exit
+    
+    # Construct f-vector
+    f_hvec = get_fvec(f_h, mesh)
+    
+    eta_lin = 1.
+    eta_disc = 0.
+    gamma_lin = 0.0000001
+    maxIter = 25; dispOutput = True
+    iterDiffArray = [0]
+    exactErrArray = [0]
+    iter = 0
+    error_estimators = np.array([[0.,0.,0.,0.,0.]])
+    JupArray = [0]
+    # Begin Picard iterations
+    while eta_lin > gamma_lin*eta_disc and iter < maxIter:
+        iter += 1
+        if solver == 'Newton':
+            A, b = assemble_system(J, -F, bcs_du)
+            solve(A, du.vector(), b)
+            u.vector()[:] += du.vector()
+        elif solver == 'Picard':
+            solve(a == L, u, bcs)
+        # calculate iterate difference and exact error in L2 norm
+        itErr = errornorm(u_k, u, 'H1')
+        exErr = errornorm(exact_solution, u, 'H1')
+        iterDiffArray.append(itErr) # fill arrays with error data
+        exactErrArray.append(exErr)
+        
+        eta_disc, eta_lin, eta_quad, eta_osc, eta_NC = get_estimators(V, f, f_h, f_hvec, sigma, sigma_lin, u, u_k, mesh, p)
+        error_estimators = np.concatenate((error_estimators, np.array([[eta_disc, eta_lin, eta_quad, eta_osc, eta_NC]])), axis = 0)
+        JupArray.append(assemble((inner(sigma(u_e) - sigma(u), sigma(u_e) - sigma(u)))**(qu/2)*dx)**(1/qu) + error_estimators[-1,4])
+        if dispOutput:
+            print('k = ' + str(iter) + ' | u-diff =  ' + str(itErr) + 
+            ', exact error = ' + str(exErr) + ', eta_lin = ' + str(eta_lin)
+            + ' , eta_disc = ' + str(eta_disc))
+        u_k.assign(u) # update for next iteration
+
+    return u, error_estimators
